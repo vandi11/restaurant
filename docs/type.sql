aa8e8eed0e68200a000a74b633298aee3af2ef73
Andi <vinczepalkoandrea@ktch.hu>
2025-11-13 19:13:55 +0100

    first laravel project

---
+++ docs/type.sql
@@ -0,0 +1,11 @@
+INSERT INTO types (type) VALUES
+('Leves'),
+('Főétel'),
+('Köret'),
+('Desszert'),
+('Saláta'),
+('Alkoholos ital'),
+('Szénsavas üdítő'),
+('Rostos üdítő'),
+('Forró ital'),
+('Sör');
